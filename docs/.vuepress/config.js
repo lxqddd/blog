09d1d77aab63ba8b945a2e3d8ddca3fe0f24f9be
向阳 <xiangqian_liu@163.com>
2022-05-30 23:18:48 +0800

    feat: 初始化

---
+++ docs/.vuepress/config.js
@@ -0,0 +1,40 @@
+module.exports = {
+  title: '向阳的技术博客',
+  description: '向阳的技术博客',
+  themeConfig: {
+    nav: [
+        { text: '首页', link: '/' },
+        { 
+            text: '向阳', 
+            items: [
+                { text: 'Github', link: 'https://github.com/lxqddd' },
+                { text: '掘金', link: 'https://juejin.cn/user/3526889035282551/posts' }
+            ]
+        }
+    ],
+    sidebar: [
+      {
+        title: '欢迎学习',
+        path: '/',
+        collapsable: false, // 不折叠
+        children: [
+          {
+            title: '学前必读',
+            path: '/'
+          }
+        ]
+      },
+      {
+        title: '基础学习',
+        path: '/handbook/Article',
+        collapsable: false,
+        children: [
+          {
+            title: '第一篇文章',
+            path: '/handbook/Article'
+          }
+        ]
+      }
+    ]
+  }
+}
